201fb9c9e667ec549d2839518d4c7d38cba84ded
pete7n <164203441+pete7n@users.noreply.github.com>
2024-03-26 15:38:17 +0000

    use module for sg

--- main.tf
+++ main.tf
@@ -29,14 +29,14 @@ vpc_security_group_ids = [module.blog_sg.security_group_id]
   }
 }
 
-module "blog_sg"" {
+module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
-  name = "blog_new"
-
+  
   vpc_id = data.aws_vpc.default.id
-
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  name = "blog_new"
+  
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules       = ["all-all"]
